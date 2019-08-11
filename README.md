48970189f42f74eca9fe0dd221b6ee0522611c89
dewaleolaoye <adewaleolaoye21@gmail.com>
2019-08-11 13:10:01 +0100

    handle delete button

---
+++ README.md
@@ -0,0 +1,8 @@
+### Learning React
+
+## I understand 
+- JSX
+- Rendering List
+- Conditional Rendering
+- Handling Events
+- Updating State
--- src/components/movie.component.jsx
+++ src/components/movie.component.jsx
@@ -5,28 +5,49 @@ class Movie extends Component {
     movies: getMovies()
   };
 
+  handleDelete = movie => {
+    const movies = this.state.movies.filter(
+      singleMovie => singleMovie._id !== movie._id
+    );
+    this.setState({ movies });
+  };
+
   render() {
+    const { length: count } = this.state.movies;
+    if (count === 0) return <p>There are no movies in the database</p>;
     return (
-      <table className='table'>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Genre</th>
-            <th>Stock</th>
-            <th>Rate</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.movies.map(movie => (
-            <tr key={movie._id}>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
+      <React.Fragment>
+        <p>Showing {count} movies in the database</p>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th />
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className='btn btn-danger btn-sm'
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
     );
   }
 }
