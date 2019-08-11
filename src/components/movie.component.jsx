aa58ab193324a863651536b0eae492f6e2cc6006
dewaleolaoye <adewaleolaoye21@gmail.com>
2019-08-11 11:51:22 +0100

    render movie list table

--- src/components/movie.component.jsx
+++ src/components/movie.component.jsx
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
-import { getMovies } from '../services/fakeMovieService';
+// import { getMovies } from '../services/fakeMovieService';
 import TableHead from './tableHead';
-
+import TableRow from './tableRow';
 class Movie extends Component {
-  state = {
-    movies: getMovies()
-  };
+  // state = {
+  //   movies: getMovies()
+  // };
 
   render() {
     // const fetchMovie = this.state.movies;
     return (
       <table className='table'>
         <TableHead />
+        <TableRow />
       </table>
     );
   }
--- src/components/tableRow.jsx
+++ src/components/tableRow.jsx
@@ -1,2 +1,33 @@
-// import React, {Component} from 'react'
-// import
+import React, { Component } from 'react';
+import { getMovies } from '../services/fakeMovieService';
+
+class Rows extends Component {
+  state = {
+    movies: getMovies()
+  };
+
+  render() {
+    const fetchMovie = this.state.movies;
+    return (
+      <React.Fragment>
+        {fetchMovie.map(movie => (
+          <tbody key={movie._id}>
+            <tr>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button className='btn bg-danger' style={{ color: '#fff' }}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        ))}
+      </React.Fragment>
+    );
+  }
+}
+
+export default Rows;
